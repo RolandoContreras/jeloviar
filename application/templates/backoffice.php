1b74d8a920b7eb183346c034cb0ccf81d8dc1faf
software.contreras@gmail.com <software.contreras@gmail.com>
2020-05-28 14:59:16 -0500

    cambios en backoffice

--- application/templates/backoffice.php
+++ application/templates/backoffice.php
@@ -3,8 +3,7 @@
     <?php $this->load->view("backoffice/head"); ?>
     <body class="logged-in stm_lms_button skin_custom_color online-light stm_preloader_1 wpb-js-composer js-comp-ver-5.6 vc_responsive" ontouchstart="">
         <div id="wrapper">
-            <?php $this->load->view("backoffice/header"); ?>
-            <!-- id header -->
+            <?php $this->load->view("header"); ?>
             <div id="main">
                 <div class="stm-lms-wrapper">
                     <div class="container">
@@ -12,22 +11,21 @@
                             <div class="col-md-3 col-sm-12">
                                 <div class="stm_lms_user_side">
                                     <div class="stm-lms-user-avatar-edit "> 
-                                        <input type="file" /> <i class="lnricons-pencil"></i>
-                                        <div class="stm-lms-user_avatar"> 
-                                            <img alt='avatar' src='<?php echo site_url() . 'assets/backoffice/images/avatar.png'; ?>' class='avatar avatar-215 photo' height='215' width='215'/> 
+                                        <div class=""> 
+                                            <img class="img-radius" alt='avatar' src='https://u-linex.com/static/backoffice/images/profile/elizabeth.jpg' class='avatar avatar-215 photo' height='215' width='215'/> 
                                         </div>
                                     </div>
-                                    <h3 class="stm_lms_update_field__first_name"><?php echo $_SESSION['customer']['name']; ?></h3>
+                                    <h3 class="stm_lms_update_field__first_name">Rolando Irvyng</h3>
                                     <div class="stm_lms_profile_buttons_set 22">
                                         <div class="stm_lms_profile_buttons_set__inner">
                                             <div class="stm-lms-user_message_btn"> <i class="stm-lms-user_message_btn__counter">1</i> 
                                                 <a href="#" class="btn btn-default">Mis Mensajes</a>
                                             </div>
                                             <div class="stm-lms-user_create_announcement_btn">
-                                                <a href="<?php echo site_url() . 'backoffice'; ?>"><i class="fa fa-home"></i><span>Inicio</span></a>
+                                                <a href="<?php echo site_url().'backoffice';?>"><i class="fa fa-home"></i><span>Inicio</span></a>
                                             </div>
                                             <div class="stm-lms-user_create_announcement_btn">
-                                                <a href="<?php echo site_url() . 'backoffice/cursos'; ?>"><i class="fa fa-shopping-cart"></i><span>Comprar Curso</span></a>
+                                                <a href="<?php echo site_url().'cursos';?>"><i class="fa fa-shopping-cart"></i><span>Comprar Curso</span></a>
                                             </div>
                                             <?php foreach ($obj_category as $value) {
                                                 if($value->backoffice == 1){?>
@@ -36,15 +34,16 @@
                                                 </div>
                                                <?php }
                                              } ?>
-
                                             <div class="stm-lms-user_edit_profile_btn" data-container=".stm_lms_edit_account">
-                                                <a href="#"> <i class="fa fa-cog"></i> <span>Editar Perfil</span> </a>
+                                                <a href="<?php echo site_url().'backoffice#settings';?>"> <i class="fa fa-cog"></i> <span>Editar Perfil</span> </a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <?php echo $body; ?>
+                            <?php echo $body;?>
+                            <script src="https://u-linex.com/static/backoffice/js/script/profile.js"></script>
+                            <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>                        
                         </div>
                     </div>
                 </div>
@@ -52,21 +51,22 @@
             <!--#main-->
         </div>
         <!--#wrapper-->
-        <?php $this->load->view("footer"); ?>
-        <link rel='stylesheet' id='stm-lms-cart-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/cart.css?ver=75'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms-lesson-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/lesson.css?ver=75'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms-user-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/user.css?ver=75'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms_categories_megamenu-style_1-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/lms_categories_megamenu.css?ver=3.2'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-header_mobile-account-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/account.css?ver=3.2'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms-user-courses-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/user-courses.css?ver=75'; ?>' type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms-edit_account-css' href='<?php echo site_url() . 'assets/backoffice/css/footer/edit_account.css?ver=75'; ?>' type='text/css' media='all' />
-        <script src='<?php echo site_url() . 'assets/backoffice/js/edit_account.js?ver=75'; ?>'></script>
-        <script src='<?php echo site_url() . 'assets/backoffice/js/bootstrap.min.js?ver=3.2'; ?>'></script>
-        <script src='<?php echo site_url() . 'assets/backoffice/js/custom.js?ver=3.2'; ?>'></script>
-        <script src='<?php echo site_url() . 'assets/backoffice/js/select2.full.min.js?ver=3.2'; ?>'></script>
-        <script src='<?php echo site_url() . 'assets/backoffice/js/jquery.fancybox.js?ver=3.2'; ?>'></script>
+        <?php $this->load->view("footer");?>
+        <link rel='stylesheet' id='stm-lms-cart-css' href='<?php echo site_url().'assets/backoffice/css/footer/cart.css?ver=75';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-lms-lesson-css' href='<?php echo site_url().'assets/backoffice/css/footer/lesson.css?ver=75';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-lms-user-css' href='<?php echo site_url().'assets/backoffice/css/footer/user.css?ver=75';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-lms_categories_megamenu-style_1-css' href='<?php echo site_url().'assets/backoffice/css/footer/lms_categories_megamenu.css?ver=3.2';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-header_mobile-account-css' href='<?php echo site_url().'assets/backoffice/css/footer/account.css?ver=3.2';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-lms-user-courses-css' href='<?php echo site_url().'assets/backoffice/css/footer/user-courses.css?ver=75';?>' type='text/css' media='all' />
+        <link rel='stylesheet' id='stm-lms-edit_account-css' href='<?php echo site_url().'assets/backoffice/css/footer/edit_account.css?ver=75';?>' type='text/css' media='all' />
+        <script src='<?php echo site_url().'assets/backoffice/js/bootstrap.min.js?ver=3.2';?>'></script>
+        <script src='<?php echo site_url().'assets/backoffice/js/jquery.fancybox.js?ver=3.2';?>'></script>
+        <script src='<?php echo site_url().'assets/backoffice/js/select2.full.min.js?ver=3.2';?>'></script>
+        <script src='<?php echo site_url().'assets/backoffice/js/custom.js?ver=3.2';?>'></script>
+        <script src='<?php echo site_url().'assets/backoffice/js/edit_account.js?ver=75';?>'></script>
+        <script src='<?php echo site_url().'assets/backoffice/js/header_2.js?ver=3.2';?>'></script>
         <script type='text/javascript'>
-            var stm_lms_edit_account_info = {"meta": {}};
+            var stm_lms_edit_account_info = {"id": "", "avatar": "", "avatar_url": "", "email": "", "url": "", "meta": {}}
         </script>
     </body>
 </html>
--- application/views/backoffice/head.php
+++ application/views/backoffice/head.php
@@ -1,14 +1,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Jeloviar Online | Plataforma Virtual</title>
+    <title>Jeloviar | Plataforma Virtual</title>
+    <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/bootstrap.min.css?ver=3.2'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' id='linear-css' href='<?php echo site_url() . 'assets/backoffice/css/linear.css?ver=2.0'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' id='linear-icons-css' href='<?php echo site_url() . 'assets/backoffice/css/linear-icons.css?ver=2.0'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' id='stm_lms_icons-css' href='<?php echo site_url() . 'assets/backoffice/css/stmlms_icons.css?ver=2.0'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/font-awesome.min.css?ver=2.0'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/stm_lms.css?ver=1588450773'; ?>' type='text/css' media='all' />
-    <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/bootstrap.min.css?ver=3.2'; ?>' type='text/css' media='all' />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/styles.css?ver=3.2'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/jquery.fancybox.css'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/animate.css'; ?>' type='text/css' media='all' />
@@ -16,11 +15,9 @@
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/header_2_transparent.css'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/custom_styles.css'; ?>' type='text/css' media='all' />
     <link rel='stylesheet' href='<?php echo site_url() . 'assets/backoffice/css/mystyle.css'; ?>' type='text/css' media='all' />
+    <script type='text/javascript' src='<?php echo site_url() . 'assets/backoffice/js/metaboxes/vue.min.js?ver=2.0'; ?>'></script>
     <link rel='stylesheet' id='redux-google-fonts-stm_option-css' href='https://fonts.googleapis.com/css?family=Montserrat%3A200%2C500%2C600%2C400%2C700%7COpen+Sans%3A300%2C400%2C600%2C700%2C800%2C300italic%2C400italic%2C600italic%2C700italic%2C800italic&#038;subset=latin&#038;ver=1586935180' type='text/css' media='all' />
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-    <script type='text/javascript' src='<?php echo site_url() . 'assets/backoffice/js/metaboxes/vue.min.js?ver=2.0'; ?>'></script>
-    <script type='text/javascript' src='<?php echo site_url() . 'assets/backoffice/js/metaboxes/vue2-datepicker.min.js?ver=2.0'; ?>'></script>
-    <script type='text/javascript' src='https://stylemixthemes.com/masterstudy/white-lms/wp-content/plugins/stm-post-type//theme/assets/lazysizes.min.js?ver=5.4.1'></script>
     <!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="https://stylemixthemes.com/masterstudy/white-lms/wp-content/plugins/js_composer/assets/css/vc_lte_ie9.min.css" media="screen"><![endif]-->
     <!-- favicon-->
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url() . 'assets/page_front/images/logo/favico/apple-touch-icon.png'; ?>">
@@ -30,10 +27,9 @@
     <!--//swetaler2-->
     <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@3/dark.css" rel="stylesheet">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
-    <link rel='stylesheet' id='stm-lms-lesson_video-css' href='https://stylemixthemes.com/masterstudy/classic-lms/wp-content/plugins/masterstudy-lms-learning-management-system/assets/css/parts/lesson_video.css?ver=29'
-              type='text/css' media='all' />
-        <link rel='stylesheet' id='stm-lms-lesson_video-css' href='<?php echo site_url().'assets/course/css/mystyle.css?ver=29';?>' type='text/css' media='all' />
+    <link rel="canonical" href="<?php echo site_url(); ?>"/>
     <script>
         var site = "<?php echo site_url(); ?>"
     </script>
+    <script src="https://checkout.culqi.com/js/v3"></script>
 </head>
